20e7fa3ff944b1ba9326c177ac9e901e4ad1d031
Om Prakash <132379517+121omprakash@users.noreply.github.com>
2024-08-21 17:38:08 +0530

    Update Lab1_IN_LAB_2_MyDemo.java

--- Lab1_IN_LAB_2_MyDemo.java
+++ Lab1_IN_LAB_2_MyDemo.java
@@ -1,4 +1,18 @@
 //Problem: Modularizing to class level of in lab q1
 import java.util.Scanner;
 public class Lab1_IN_LAB_2_MyDemo{
-  
+  public static void main(String Args[])
+  {
+    System.out.println("Enter a number: ");
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    System.out.println("Factorial of "+n+" = "+ MyintegerMath.factorial(n));
+    if(MyintegerMath.isStrong(n))
+    {
+      System.out.println(n+" is Strong number");
+    }
+    else
+    {
+      System.out.println(n+ " is not a strong number");
+    }
+  }
