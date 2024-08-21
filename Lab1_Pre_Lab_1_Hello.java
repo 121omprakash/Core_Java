c6aa86e4cd35fdd5d231d2fd115a38cb812c89c8
Om Prakash <132379517+121omprakash@users.noreply.github.com>
2024-08-21 16:37:10 +0530

    Update and rename Hello.java to Lab1_Pre_Lab_1_Hello.java

---
+++ Lab1_Pre_Lab_1_Hello.java
@@ -0,0 +1,8 @@
+// Problem: Write a java program to print the "Hello world" Statement.
+
+public class Lab1_Pre_Lab_1_Hello {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+}
